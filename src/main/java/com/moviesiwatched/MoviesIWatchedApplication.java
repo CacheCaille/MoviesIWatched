41754d4bec3168fb6f4d952409f08c2dc338aeda
CacheCaille <zoolk.gabi@gmail.com>
2024-06-24 00:39:19 +0200

    First Commit

---
+++ src/main/java/com/moviesiwatched/MoviesIWatchedApplication.java
@@ -0,0 +1,13 @@
+package com.moviesiwatched;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesIWatchedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviesIWatchedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moviesiwatched/MoviesIWatchedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviesiwatched;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesIWatchedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
